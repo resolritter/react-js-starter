e861ae6d5259b4eabd038f89e39f4e9710f22437
resolritter <resolritter@users.noreply.github.com>
2020-06-08 09:02:05 -0300

    add IncrementButton variants with different state management strategies

---
+++ src/components/StoreIncrementButton.js
@@ -0,0 +1,21 @@
+import React from "react"
+import IncrementButton from "./IncrementButton.js"
+import { useDispatch, useSelector } from "react-redux"
+import { get } from "lodash/fp"
+import { increment } from "src/store/counter"
+
+export function StoreIncrementButton() {
+  const count = useSelector(get("counter"))
+  const dispatch = useDispatch()
+  return (
+    <IncrementButton
+      increment={function () {
+        dispatch(increment())
+      }}
+      count={count}
+      title={"Increment counter (Redux state)"}
+    />
+  )
+}
+
+export default StoreIncrementButton
