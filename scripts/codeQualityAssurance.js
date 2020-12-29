4279d1f3e3091c3446fe22a0974d3bc057c2bf9c
resolritter <resolritter@users.noreply.github.com>
2020-12-29 15:42:28 -0300

    consider renamed files in lint script

--- scripts/codeQualityAssurance.js
+++ scripts/codeQualityAssurance.js
@@ -54,6 +54,39 @@ if (command.endsWith("all")) {
   process.exit(0)
 }
 
+const parseGitStatusLine = function (line, delimiter = '"') {
+  if (line[0] !== delimiter) {
+    const indexOfRenameSign = line.indexOf(" -> ")
+    return indexOfRenameSign > -1
+      ? {
+          path: line.slice(0, indexOfRenameSign),
+          read: indexOfRenameSign,
+        }
+      : {
+          path: line,
+          read: line.length,
+        }
+  }
+
+  let skipEscaped = false
+  let i = 1
+  for (; i < line.length; ++i) {
+    if (line[i] === "\\") {
+      skipEscaped = true
+      line = `${line.slice(0, i)}${line.slice(i + 1)}`
+      --i
+    } else if (line[i] === delimiter) {
+      if (skipEscaped) {
+        skipEscaped = false
+      } else {
+        break
+      }
+    }
+  }
+
+  return { path: line.slice(1, i), read: i + 1 }
+}
+
 let files
 if (args.length) {
   console.log("[INFO] operating on argument files")
@@ -64,8 +97,8 @@ if (args.length) {
     .toString()
     .trim()
     .split("\n")
-    .filter(function (file) {
-      return file[0] == "A" || file[0] == "M"
+    .filter(function (line) {
+      return line[0] === "A" || line[0] === "M" || line.slice(0, 2) === "R "
     })
 
   if (stagedFilesOutput.length) {
@@ -83,7 +116,37 @@ if (args.length) {
         } else if (isWhitespacing) {
           isWhitespacing = false
           if (whitespaceCount === 1) {
-            return line.slice(i)
+            let remainder = line.slice(i)
+            const { path: origin, read } = parseGitStatusLine(remainder)
+            if (line[0] === "R") {
+              /*
+              When a file is renamed, the line output is like "previous -> next".
+              Therefore, the string should be split at the " -> " position and
+              the "next" location will be used for the linters.
+              However, the file name itself might have the " -> " sequence,
+              in which case the file names' output will be quoted. The quotes
+              will serve to disambiguate where the previous and new paths
+              begin and end.
+
+              Case 1: renaming a file "a -> b" to "a -> c" will yield the
+              following:
+                 R  "a -> b" -> "a -> c"
+              The middle arrow will not be quoted, thus it's possible to
+              know where each path begins and ends.
+
+              Case 2: renaming a file "a->b" to "a->c" will yield the
+              following:
+                 R  a->b -> a->c
+              There can't be occurrences of " -> " inside of the previous path,
+              otherwise it'd be quoted.
+              */
+
+              remainder = remainder.slice(read + " -> ".length)
+              const { path: destination } = parseGitStatusLine(remainder)
+              return destination
+            } else {
+              return origin
+            }
           }
         }
       }
